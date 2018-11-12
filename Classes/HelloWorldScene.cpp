7a890916919098e68ca479a27b8326d7648e0b4d
Patricio Palma Contreras <patriciop@gmail.com>
2018-11-12 11:45:26 -0300

    Added props to main screen

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -1,51 +1,49 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-
 #include "HelloWorldScene.h"
 #include "SimpleAudioEngine.h"
 #include "PauseScene.h"
 
 USING_NS_CC;
 
-Scene* HelloWorld::createScene()
-{
-    return HelloWorld::create();
-}
-
 // Print useful error message instead of segfaulting when files are not there.
 static void problemLoading(const char* filename)
 {
     printf("Error while loading: %s\n", filename);
-    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+    printf("Depending on how you compiled you might have to add 'Resources/' "
+            "in front of filenames in HelloWorldScene.cpp\n");
+}
+
+Scene* HelloWorld::createScene()
+{
+//    auto scene = HelloWorld::create();
+    auto scene = HelloWorld::createWithPhysics();
+    auto world = scene->getPhysicsWorld();
+    world->setGravity(Vec2(0,0));
+    world->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
+
+    return scene;
+}
+
+HelloWorld* HelloWorld::createWithPhysics()
+{
+    HelloWorld *ret = new (std::nothrow) HelloWorld();
+    if (ret && ret->initWithPhysics())
+    {
+        ret->autorelease();
+        return ret;
+    }
+    else
+    {
+        CC_SAFE_DELETE(ret);
+        return nullptr;
+    }
 }
 
 // on "init" you need to initialize your instance
-bool HelloWorld::init()
+bool HelloWorld::initWithPhysics()
 {
     //////////////////////////////
     // 1. super init first
-    if ( !Scene::init() )
+    if ( !Scene::initWithPhysics() )
     {
         return false;
     }
@@ -66,9 +64,9 @@ bool HelloWorld::init()
 //                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
     
     auto closeItem = MenuItemImage::create(
-                                          "CloseNormal.png",
-                                          "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::pauseCallback, this));
+            "pause.png",
+            "pause_pressed.png",
+            CC_CALLBACK_1(HelloWorld::pauseCallback, this));
 
     if (closeItem == nullptr ||
         closeItem->getContentSize().width <= 0 ||
@@ -109,26 +107,47 @@ bool HelloWorld::init()
         this->addChild(label, 1);
     }
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-    if (sprite == nullptr)
-    {
-        problemLoading("'HelloWorld.png'");
-    }
-    else
-    {
+    // add "HelloWorld" logo
+    if( auto sprite = Sprite::create("HelloWorld.png") ) {
         // position the sprite on the center of the screen
         sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
 
         // add the sprite as a child to this layer
         this->addChild(sprite, 0);
+
+        sprite->runAction(FadeOut::create(1.0));
     }
 
-
     if( auto bg = Sprite::create("background.png") ) {
-        bg->setPosition(origin.x + bg->getContentSize().width / 2,
+        bg->setPosition(
+                origin.x + bg->getContentSize().width / 2,
                 origin.y + bg->getContentSize().height /2);
         this->addChild(bg, -1);
+    }
+
+    if( auto sprBomb = Sprite::create("bomb.png") ) {
+        sprBomb->setPosition(
+                origin.x + visibleSize.width / 2,
+                origin.y + visibleSize.height + sprBomb->getContentSize().height / 2);
+                this->addChild(sprBomb,1);
+    }
+
+    if( auto sprPlayer = Sprite::create("player.png") ) {
+        sprPlayer->setPosition(
+                origin.x + visibleSize.width / 2,
+                origin.y + visibleSize.height * 0.21);
+        this->addChild(sprPlayer, 0);
+
+        Vector<SpriteFrame*> frames;
+        Size playerSize = sprPlayer->getContentSize();
+        frames.pushBack(SpriteFrame::create(
+                "player.png", Rect(0, 0, playerSize.width, playerSize.height)));
+        frames.pushBack(SpriteFrame::create(
+                "player2.png", Rect(0, 0, playerSize.width, playerSize.height)));
+
+        auto animation = Animation::createWithSpriteFrames( frames, 0.2f );
+        auto animate = Animate::create( animation );
+        sprPlayer->runAction(RepeatForever::create(animate));
     }
 
     return true;
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -31,14 +31,15 @@ class HelloWorld : public cocos2d::Scene
 {
 public:
     static cocos2d::Scene* createScene();
+    static HelloWorld* createWithPhysics();
 
-    virtual bool init();
+    virtual bool initWithPhysics();
     
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
     
     // implement the "static create()" method manually
-    CREATE_FUNC(HelloWorld);
+//    CREATE_FUNC(HelloWorld);
     void pauseCallback(cocos2d::Ref* pSender);
 };
 
